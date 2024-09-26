1d3ebc7995acb7abc53984ff86beb65c1ddbb7a4
BGunashree <gunashreeb02.xworkz@gmail.com>
2024-09-26 21:27:23 +0530

    exception handling

---
+++ exception/runner/ExceptionRunner.java
@@ -0,0 +1,18 @@
+package com.xworkz.events.exception.runner;
+
+import com.xworkz.events.exception.InvalidEmailException;
+import com.xworkz.events.exception.ServiceImpl;
+
+public class ExceptionRunner {
+
+	public static void main(String[] args) throws InvalidEmailException {
+		
+		ServiceImpl serviceImpl=new ServiceImpl();
+		serviceImpl.saveAmount(100000);
+		serviceImpl.saveEmail("gunashreebyanna@gmail.omc");
+		
+		
+
+	}
+
+}
